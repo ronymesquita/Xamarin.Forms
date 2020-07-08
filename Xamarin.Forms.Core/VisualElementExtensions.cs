82751d04972be8617b51b38825896c700df12087
Stephane Delcroix <stephane@delcroix.org>
2020-07-08 18:12:55 +0200

    Drop AppTheme flag

--- Xamarin.Forms.Core/VisualElementExtensions.cs
+++ Xamarin.Forms.Core/VisualElementExtensions.cs
@@ -5,11 +5,7 @@ namespace Xamarin.Forms
 {
 	public static class VisualElementExtensions
 	{
-		public static void SetOnAppTheme<T>(this VisualElement self, BindableProperty targetProperty, T light, T dark)
-		{
-			ExperimentalFlags.VerifyFlagEnabled(nameof(BindableObjectExtensions), ExperimentalFlags.AppThemeExperimental, nameof(BindableObjectExtensions), nameof(SetOnAppTheme));
-			self.SetBinding(targetProperty, new AppThemeBinding { Light = light, Dark = dark});
-		}
+		public static void SetOnAppTheme<T>(this VisualElement self, BindableProperty targetProperty, T light, T dark) => self.SetBinding(targetProperty, new AppThemeBinding { Light = light, Dark = dark });
 
 		public static void SetAppThemeColor(this VisualElement self, BindableProperty targetProperty, Color light, Color dark) => SetOnAppTheme(self, targetProperty, light, dark);
 	}
